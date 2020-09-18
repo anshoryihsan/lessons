c0d0d37689ef149db8636cbad575842488998e6b
irvanster <tovamaulana098@gmail.com>
2020-09-18 15:04:11 +0700

    push

--- week2/function.js
+++ week2/function.js
@@ -1,9 +1,8 @@
-const nama = 'Anjas'
-
+// const nama = 'Anjas'
 
 // function mengambilNama(aha) {
 //     console.log('nama:', aha)
-    
+
 // }
 
 // const mengambilNama = function(nama) {
@@ -24,17 +23,50 @@ const nama = 'Anjas'
 //     )
 // }
 
-
-
-
-
-
 // mengambilNama(nama)
 
 // const nilai = {
 //     bahasaIndonesia: 50,
 //     bahasaInggris: 20,
 // }
+// const nilai = [
+//     {
+//         pelajaran: 'Bahasa Indonesia',
+//         nilai: 50
+//     },
+//     {
+//         pelajaran: 'Bahasa Inggris',
+//         nilai: 20
+//     },
+//     {
+//         pelajaran: 'IPA',
+//         nilai: 60
+//     },
+// ]
+// const additional = (nilai) => {
+//     // console.log(
+//         nilai.map((item, index) => {
+//             console.log(`nilai: ${item.nilai}`)
+//         })
+//     // )
+// }
+
+// additional(nilai)
+
+// var obj = {
+//   nama: "gg",
+//   nama2: "hh",
+// };
+
+// function cari(x) {
+//   console.log(
+//     obj.nama == x || obj.nama2 == x ? x : "pencarian tydac ditemukan"
+//   );
+// }
+
+// cari("adasd");
+
+
 const nilai = [
     {
         pelajaran: 'Bahasa Indonesia',
@@ -49,12 +81,10 @@ const nilai = [
         nilai: 60
     },
 ]
-const additional = (nilai) => {
-    // console.log(
-        nilai.map((item, index) => {
-            console.log(`nilai: ${item.nilai}`)
-        })
-    // )
+const pencarian = (kataKunci) => {
+    // let filter = nilai.filter((item, index) => item.pelajaran == kataKunci)
+    let filter = nilai.filter((item, index) => item.pelajaran == kataKunci)
+    console.log(filter)
 }
 
-additional(nilai)
+pencarian('IPA')
